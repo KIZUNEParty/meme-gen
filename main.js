58e2385bc983052c298233e31f9c0e901fc34c7c
ProfDetermined <owner@detzz.in.th>
2021-10-09 19:17:22 +0000

    rm canvas part

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import './style.css'
-// const { createCanvas, loadImage } = require('canvas')
 
 document.querySelector('#app').innerHTML = `
   <h1><i style="color: #F8F835;">คำคมสอนมึง Generator</i></h1>
-  <canvas id="outP" width="600" height="600" style="border:1px solid whitesmoke;">
+  <canvas id="outP" width="`+ width +`" height="`+ height +`" style="border:1px solid whitesmoke;">
   </canvas>
 `
