255fe948f989d829fbbbab642d8e437ffe01ad55
ProfDetermined <owner@detzz.in.th>
2021-10-09 17:02:55 +0000

    add canvas part

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const { createCanvas, loadImage } = require('canvas')
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
-  <canvas id="outP" width="200" height="100" style="border:1px solid #000000;">
+  <canvas id="outP" width="200" height="100" style="border:1px solid black;">
   </canvas>
 `
