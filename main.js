045e7936df103d38398df43e59975604a656c463
ProfDetermined <owner@detzz.in.th>
2021-10-09 20:59:05 +0000

    adding img loading

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ const width = "600";
 const height = "600";
 
 document.querySelector('#app').innerHTML = `
+
   <h1><i style="color: #F8F835;">คำคมสอนมึง Generator</i></h1>
+  <form>
+    <input type="text" placeholder="img URL" id="uURI" name="uURI">
+    <button type="submit">Submit</button>
+  </form>
+  <br />
   <canvas id="canvas" width="`+ width +`" height="`+ height +`" style="border:1px solid whitesmoke;">
   </canvas>
 `
+
+const URI = document.getElementById('uURI').value
 
 // Canvas Part
 const canvas = document.getElementById('canvas')
@@ -17,6 +25,11 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = '#fff'
 ctx.fillRect(0,0, width, height)
 
+const img = new Image
+img.onload = function() {
+  ctx.drawImage(img,0,0);
+}
+img.src = URI
 
 // Gradient
 const grd = ctx.createLinearGradient(0, 600, 0, 0);
