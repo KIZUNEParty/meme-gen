343021159e691ed80538f051ce88ac2a5dd422ca
ProfDetermined <owner@detzz.in.th>
2021-10-09 17:15:49 +0000

    add some color

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css'
-const { createCanvas, loadImage } = require('canvas')
+// const { createCanvas, loadImage } = require('canvas')
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1><i style="color: #F8F835;">คำคมสอนมึง Generator</i></h1>
   <canvas id="outP" width="200" height="100" style="border:1px solid black;">
   </canvas>
 `
